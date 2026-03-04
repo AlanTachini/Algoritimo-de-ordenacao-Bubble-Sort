76e47981f6a876eabc4068f7c2929988b15a3d04
alantachini <alan.arlindo1234@gmail.com>
2026-03-03 23:16:16 -0300

    refactor: renomeando palavra para dado

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,16 @@
-function ordenarPalavra(palavra){
-  console.log(palavra)
-  palavra = palavra.toString().split("")
-  for (let i = 0; i < palavra.length - 1; i++){
-    for (let j = 0; j < palavra.length - 1 - i; j++) {
-      if (palavra[j].toLowerCase() > palavra[j+1].toLowerCase()) {
-        let aux = palavra[j];
-        palavra[j] = palavra[j+1];
-        palavra[j+1] = aux;
+function ordenarDado(dado){
+  console.log(dado)
+  dado = dado.toString().split("")
+  for (let i = 0; i < dado.length - 1; i++){
+    for (let j = 0; j < dado.length - 1 - i; j++) {
+      if (dado[j].toLowerCase() > dado[j+1].toLowerCase()) {
+        let aux = dado[j];
+        dado[j] = dado[j+1];
+        dado[j+1] = aux;
       }
     }
   }
-  return palavra.join("");
+  return dado.join("");
 }
 
-console.log(ordenarPalavra("eba"));
+console.log(ordenarDado("eba"));
